8a9b91222c82780f695f4507b7b73cc486892761
Xiaohan Chen <xiaohanc@usc.edu>
2016-11-24 14:18:31 -0800

    adding finished miracle pills project

---
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Xiaohan Chen on 11/24/16.
+//  Copyright © 2016 Xiaohan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerButton: UIButton!
+    
+    let states = ["Alaska", "Arkansas", "Alabama", "Washington", "California", "Utah"]
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func stateButtonPressed(_ sender: Any) {
+        statePicker.isHidden = false
+    }
+    
+    
+    // picker functions
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerButton.setTitle(states[row], for: UIControlState())
+        statePicker.isHidden = true
+    }
+
+
+}
+
